9bc14ca5e114c96e4f22d64413834f49013e8a33
Dilshan Senarath <dilshan99news@gmail.com>
2023-01-06 22:32:27 +0530

    create json files for each song

---
+++ Corpus/code/create_json_files.js
@@ -0,0 +1,41 @@
+var fs = require('fs');
+
+required_json_structure = {
+    "title": "",
+    "released_year": "",
+    "album": "",
+    "genre": "",
+    "lyrics": "",
+    "lyricist": "",
+    "musician": "",
+    "singer": "",
+    "mp3_listen": {
+        "songhub": "",
+        "spotify": "",
+        "apple": "",
+    },
+    "video_watch": "",
+    "download": "",
+    "metaphor": [
+        {
+            "lyrics_part": "",
+            "meaning": "",
+            "type": "",
+            "source_domain": "",
+            "target_domain": "",
+        }
+    ],
+};
+
+let jsonString = JSON.stringify(required_json_structure);
+
+// Read the selected_songs.txt file
+let songs = fs.readFileSync("../selected_songs.txt").toString();
+songs = songs.split("\n");
+
+// Write json structure for files
+let counter = 0;
+for (let song_title of songs) {
+    counter = counter + 1;
+    fs.writeFileSync(`../all_songs/${counter}_${song_title}.json`, jsonString);
+}
