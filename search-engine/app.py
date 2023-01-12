0827e2272f938fee66c307b52f8a22b678ad9af1
Dilshan Senarath <dilshan99news@gmail.com>
2023-01-12 19:35:11 +0530

    add home page

---
+++ search-engine/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html',response=None)
+
+# @app.route('/search',methods=["POST"])
+# def search():
+#     res = []
+#     if(request.method == "POST"):
+#         query = request.form['query']
+#         res = QueryProcessor.processQ(query)
+#         print("Got %d Hits:" % res['hits']['total']['value'])
+#     return render_template('index.html',response=res)
+ 
+if __name__ == '__main__':
+    app.DEBUG = True
+    app.run()
